d80116795168045b694dda0eb3bd85e09325d5de
Ashyam <ashyam.zubair@algonox.com>
2019-09-26 18:46:44 +0530

    DB config and logging imporvements

--- digital_signature_api/BL/consumer.py
+++ digital_signature_api/BL/consumer.py
@@ -1,19 +1,23 @@
 import json
+import os
+import requests
 
 from kafka import KafkaConsumer, TopicPartition
+from py_zipkin.zipkin import zipkin_span,ZipkinAttrs, create_http_headers_for_new_span
 
 from db_utils import DB
 from digital_signature import is_pdf_signed
 from producer import produce
-
-try:
-    from app.ace_logger import Logging
-except:
-    from ace_logger import Logging
-
-from py_zipkin.zipkin import zipkin_span,ZipkinAttrs, create_http_headers_for_new_span
+from ace_logger import Logging
 
 logging = Logging()
+
+db_config = {
+    'host': os.environ['HOST_IP'],
+    'user': os.environ['LOCAL_DB_USER'],
+    'password': os.environ['LOCAL_DB_PASSWORD'],
+    'port': os.environ['LOCAL_DB_PORT']
+}
 
 def http_transport(encoded_span):
     body = encoded_span
@@ -24,29 +28,7 @@ def http_transport(encoded_span):
 
 def consume(broker_url='broker:9092'):
     try:
-        common_db_config = {
-            'host': 'common_db',
-            'port': '3306',
-            'user': 'root',
-            'password': 'root'
-        }
-        kafka_db = DB('kafka', **common_db_config)
-        # kafka_db = DB('kafka')
-
-        extraction_db_config = {
-            'host': 'extraction_db',
-            'port': '3306',
-            'user': 'root',
-            'password': 'root'
-        }
-        extraction_db = DB('extraction', **extraction_db_config)
-
-        overwrite = True
-
-        message_flow = kafka_db.get_all('message_flow')
-        listen_to_topic_df = message_flow.loc[message_flow['listen_to_topic'] == 'digital_signature']
-        topic = list(listen_to_topic_df.listen_to_topic)[0]
-        send_to_topic = list(listen_to_topic_df.send_to_topic)[0]
+        topic = 'digital_signature'
 
         logging.info(f'Listening to topic `{topic}`...')
         consumer = KafkaConsumer(
@@ -87,41 +69,49 @@ def consume(broker_url='broker:9092'):
 
         for message in consumer:
             data = message.value
+            
+            if not isinstance(data, dict):
+                logging.warning(f'Received data is not a dictionary. [{data}]')
+                consumer.commit()
+                continue
 
             if not data:
                 logging.info(f'Got empty data. {data}')
                 consumer.commit()
                 continue
-
-            tenant_id = data['tenant_id']
+                
+            case_id = data.get('case_id', None)
+            tenant_id = data.get('tenant_id', None)
             zipkin_headers = data['zipkin_headers']
-            case_id = data.pop('case_id', None)
+            
+            if case_id is None:
+                logging.debug(f'Recieved unknown data. [{data}]')
+                consumer.commit()
+
+            kafka_db = DB('kafka', tenant_id=tenant_id, **db_config)
+            extraction_db = DB('extraction', tenant_id=tenant_id, **db_config)
+
+            query = 'SELECT * FROM `message_flow` WHERE `listen_to_topic`=%s'
+            message_flow = kafka_db.execute(query, params=[topic])
+            send_to_topic = list(message_flow.send_to_topic)[0]
+
             with zipkin_span(service_name='digital_signature', span_name='consumer', 
-                zipkin_attrs=ZipkinAttrs(trace_id=zipkin_headers['X-B3-TraceId'],
-                span_id=zipkin_headers['X-B3-SpanId'],
-                parent_span_id=zipkin_headers['X-B3-ParentSpanId'],
-                flags=zipkin_headers['X-B3-Flags'],
-                is_sampled=zipkin_headers['X-B3-Sampled'],),
-                transport_handler=http_transport,
-                # port=5014,
-                sample_rate=0.5,):
+                    zipkin_attrs=ZipkinAttrs(trace_id=zipkin_headers['X-B3-TraceId'],
+                    span_id=zipkin_headers['X-B3-SpanId'],
+                    parent_span_id=zipkin_headers['X-B3-ParentSpanId'],
+                    flags=zipkin_headers['X-B3-Flags'],
+                    is_sampled=zipkin_headers['X-B3-Sampled'],),
+                    transport_handler=http_transport,
+                    # port=5014,
+                    sample_rate=0.5,):
 
-                if case_id is None:
-                    logging.debug(f'Recieved unknown data. [{data}]')
-                    consumer.commit()
-
-                validation_df = extraction_db.get_all('validation')
-                case_id_validation = validation_df.loc[validation_df['case_id'] == case_id]
-                if case_id_validation.empty or overwrite:
-                    file_name = data.pop('file_name', None)
-                    response_data = is_pdf_signed(case_id, file_name)
-                    if response_data['flag'] == True:
-                        produce(send_to_topic, data)
-                        logging.info('Message commited!')
-                    else:
-                        logging.warning('Message not consumed. Some error must have occured. Will try again!')
+                file_name = data.get('file_name', None)
+                response_data = is_pdf_signed(case_id, file_name)
+                if response_data['flag'] == True:
+                    produce(send_to_topic, data)
+                    logging.info('Message commited!')
                 else:
-                    logging.debug("Consuming old message.")
+                    logging.warning('Message not consumed. Some error must have occured. Will try again!')
                 consumer.commit()
     except:
         logging.exception('Something went wrong in consumer. Check trace.')
