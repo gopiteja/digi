d79aef4c9e692799a23245e686cdb87f4d796a1e
ashish-algonox <ashish.khan@algonox.com>
2019-09-27 17:57:16 +0530

    Update queue_api.py

--- queue_api/BL/app/queue_api.py
+++ queue_api/BL/app/queue_api.py
@@ -717,12 +717,12 @@ def get_queue(queue_id=None):
                         query = "select column_name from column_definition where date = 1"
                         columns_to_change = list(db.execute_(query).column_name)
 
-                        for column in columns_to_change:
-                            try:
-                                document[column] = (document[column]).strftime(r'%x %X')
-                            except:
-                                logging.exception(f'Could not parse {column} value. `{column}` might not be mapped for the queue `{queue_name}`.')
-                                pass
+                        # for column in columns_to_change:
+                        #     try:
+                        #         document[column] = (document[column]).strftime(r'%B %d, %Y %I:%M %p')
+                        #     except:
+                        #         logging.exception(f'Could not parse {column} value. `{column}` might not be mapped for the queue `{queue_name}`.')
+                        #         pass
 
                 columns = [col for col in columns if col not in util_columns]
                 columns += extraction_columns_list
