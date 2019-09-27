d8d3ba847ba53c412d23bbc27597c26c0cc547e1
akshatgoyalalgo <akshat.goyal@lagonox.com>
2019-09-27 19:52:50 +0530

    zipkin attribute removal

--- extraction_api/BL/app/extraction_api.py
+++ extraction_api/BL/app/extraction_api.py
@@ -627,6 +627,16 @@ def value_extract(result, api=False, retrained=False):
 
     # Update queue of the file. Field exceptions if there are any suspicious value else Verify queue.
     is_field_exception = False
+
+    # query = 'SELECT * FROM `workflow_definition`, `queue_definition` WHERE ' \
+    #             '`workflow_definition`.`queue_id`=`queue_definition`.`id` '
+    # template_exc_wf = queue_db.execute(query)
+    # move_to_queue_id = list(template_exc_wf.loc[template_exc_wf['name'] == 'Template Exceptions']['move_to'])[0]
+    # query = 'SELECT * FROM `queue_definition` WHERE `id`=%s'
+    # move_to_queue_df = queue_db.execute(query, params=[move_to_queue_id])
+    # move_to_queue = list(move_to_queue_df['uniquq_name'])[0]
+
+
     updated_queue = 'Field Exceptions' if is_field_exception else 'Verify'
     query = "UPDATE `process_queue` SET `queue`=%s WHERE `case_id`=%s"
     params = [updated_queue, case_id]
@@ -1374,7 +1384,7 @@ def keyword_selector_cluster_method(ocr_data, keyword, inp, field_data, page, co
     char_index_list, haystack = get_pre_process_char(pre_process_char, ocr_data, page_no)
 
     # Search OCR for the key pattern
-    print('keywords - ', keyList)
+    # print('keywords - ', keyList)
     ori_keyList = keyList
 
     keyCords_list, keywords_list, counter = compute_all_key_list_coord(keyList, char_index_list, haystack)
@@ -1384,8 +1394,8 @@ def keyword_selector_cluster_method(ocr_data, keyword, inp, field_data, page, co
 
         keyList = ' '.join(keyList)
 
-        print(keyList)
-        print(keyCords)
+        # print(keyList)
+        # print(keyCords)
         box_top = keyCords['top'] + rel_top
         box_bottom = keyCords['bottom'] + rel_bottom
         box_left = keyCords['left'] + rel_left
