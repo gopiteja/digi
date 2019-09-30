7e5f37c2c847eb07e1cdf4618e73e987e558485f
Akhil <akhil2.new@gmail.com>
2019-10-01 00:03:56 +0530

    Added data tables configuration

--- button_functions/app/run_business_rule/apply_business_rule.py
+++ button_functions/app/run_business_rule/apply_business_rule.py
@@ -7,7 +7,7 @@ import json
 import os
 from db_utils import DB 
 
-
+from business_rules.BusinessRules import BusinessRules
 
 # one configuration
 db_config = {
@@ -17,26 +17,7 @@ db_config = {
     'port': os.environ['LOCAL_DB_PORT']
 }
 
-def get_tables_data(tenant_id, unique_column=None, unique_column_value=None, db_tables={}):
-    """Get the tables data from the DB. 
-    As of now some hardcodings and assumed things are there
-    Waiting for the ace-builder...
-    """
-    to_return = {}
-
-    for key, tables in db_tables.items():
-        db = DB(key, tenant_id=tenant_id, **db_config)
-        for table in tables:
-            if unique_column and unique_column_value:
-                query = f'SELECT * from `{table}` WHERE `{unique_column}` = "{unique_column_value}"'
-            df = db.execute(query)
-            if not df.empty:
-                to_return[table] = df.to_dict(orient='records')[0]
-            else:
-                to_return[table] = {}
-    return to_return
-
-def get_data_sources(tenant_id, case_id):
+def get_data_sources(tenant_id, case_id, master=False):
     """Helper to get all the required table data for the businesss rules to apply
     """
     get_datasources_query = "SELECT * from `data_sources`"
@@ -50,9 +31,13 @@ def get_data_sources(tenant_id, case_id):
     for database, tables in case_id_based_sources.items():
         db = DB(database, tenant_id=tenant_id, **db_config)
         for table in tables:
-            query = f"SELECT * from `{table}` WHERE case_id = %s"
-            params = [case_id]
-            df = db.execute(query, params=params)
+            if master:
+                query = f"SELECT * from `{table}`"
+                df = db.execute(query)
+            else:
+                query = f"SELECT * from `{table}` WHERE case_id = %s"
+                params = [case_id]
+                df = db.execute(query, params=params)
             if not df.empty:
                 data[table] = df.to_dict(orient='records')[0]
             else:
@@ -70,6 +55,16 @@ def get_rules(tenant_id, group):
     params = [group]
     rules = business_rules_db.execute(get_rules_query, params=params)
     return rules
+
+
+def run_group_rules(case_id, rules, data):
+    """Run the rules"""
+    rules = [json.loads(rule) for rule in list(rules['rule_string'])] 
+    BR  = BusinessRules(case_id, rules, data)
+    updates = BR.evaluate_business_rules()
+    
+    logging.info(f"\n updates from the group rules are \n{updates}\n")
+    return updates
 
 def apply_business_rule(case_id, function_params, tenant_id):
     """Run the business rules based on the stage in function params and tenant_id
@@ -99,7 +94,7 @@ def apply_business_rule(case_id, function_params, tenant_id):
 
         # making it generic takes to take a type parameter from the database..
         # As of now make it (all others  or chained) only
-        is_chain_rule = '' in rule_id_mapping
+        is_chain_rule = '' not in rule_id_mapping
         
         # get the required table data on which we will be applying business_rules  
         data_tables = get_data_sources(tenant_id, case_id) 
@@ -109,7 +104,12 @@ def apply_business_rule(case_id, function_params, tenant_id):
         # get the master data if needed
         
         # apply business rules
-        
+        if is_chain_rule:
+            # updates = run_chained_rules()
+            pass
+        else:
+            updates = run_group_rules(case_id, rules, data_tables)
+            
         
         # update in the database, the changed fields eventually when all the stage rules were got
         
