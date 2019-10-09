826105c2aedb8ddd498f991a2117e3422ba80ba3
Ashyam <ashyam.zubair@algonox.com>
2019-10-09 17:21:32 +0530

    Update folder_monitor_api.py

--- folder_monitoring_api/BL/folder_monitor_api.py
+++ folder_monitoring_api/BL/folder_monitor_api.py
@@ -111,7 +111,7 @@ def folder_monitor():
 
         for _, row in input_config.iterrows():
             input_path = row['access_1']
-            output_path = list(output_config.ix[row['output']]['access_1'])
+            output_path = output_config.ix[row['output']]['access_1']
             workflow = row['workflow']
 
             logging.debug(f'Input path: {input_path}')
