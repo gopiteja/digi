0fa5137827ecc3b3bd323eb8e3ffc2db1287712c
akshatgoyalalgo <akshat.goyal@lagonox.com>
2019-10-18 12:26:56 +0530

    abby sdk

--- template_detection/BL/AbbySDK/Make.sh
+++ template_detection/BL/AbbySDK/Make.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 #javac -classpath "src/.:libs/abbyy.FREngine.jar:libs/mysql-connector-java-8.0.17.jar" -d bin src/com/algonox/abbyy/data/*.java
 #javac -classpath "src/.:libs/abbyy.FREngine.jar:libs/mysql-connector-java-8.0.17.jar" -d bin src/com/algonox/abbyy/stream/*.java
-javac -classpath "src/.:libs/abbyy.FREngine.jar:libs/mysql-connector-java-8.0.17.jar" -d bin src/com/algonox/abbyy/*.java
+javac -classpath "src/.:lib/abbyy.FREngine.jar:lib/mysql-connector-java-8.0.17.jar" -d bin src/com/algonox/abbyy/*.java
