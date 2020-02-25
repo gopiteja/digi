793523f9f21a354b24625f5b27d9af1907c55db4
akshatgoyalalgo <akshat.goyal@lagonox.com>
2020-02-25 17:38:45 +0530

    abby sdk test

--- template_detection/BL/AbbySDK/blobby.py
+++ template_detection/BL/AbbySDK/blobby.py
@@ -68,6 +68,7 @@ def file_ocr():
         # inp = './Run.sh ' + file_name
         # inp = "/usr/bin/java -classpath '.:bin/.:libs/abbyy.FREngine.jar:libs/mysql-connector-java-8.0.17.jar' com.algonox.abbyy.OCRExtraction " + case_id
         whole_load = subprocess.check_output(['./Run.sh', command]).decode('utf-8').replace('\\r\\n', '')
+        print(whole_load)
         # whole_load = ast.literal_eval(whole_load)
         with open('/home/ubuntu/oasis-main/template_detection/BL/AbbySDK/ocr_file.pdf','rb') as f:
             blob = base64.b64encode(f.read())
