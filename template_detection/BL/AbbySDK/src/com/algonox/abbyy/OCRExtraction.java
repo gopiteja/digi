0ea32f30a6d1fd74dc9aeef629a7fe0df6de1a07
akshatgoyalalgo <akshat.goyal@lagonox.com>
2020-02-25 17:36:41 +0530

    abby sdk test

--- template_detection/BL/AbbySDK/src/com/algonox/abbyy/OCRExtraction.java
+++ template_detection/BL/AbbySDK/src/com/algonox/abbyy/OCRExtraction.java
@@ -45,7 +45,8 @@ public class OCRExtraction {
 			String fileName = null;
 			List<DocPage> pages = null;
 			try {
-			    System.out.println(json);
+			    //System.out.println("json parser");
+			    System.out.println("*********"+json+"*********");
 				JSONObject jsonObj = (JSONObject) parser.parse(json);
 				fileName = (String) jsonObj.get("fileName");
 				Object obj = jsonObj.get("pages");
