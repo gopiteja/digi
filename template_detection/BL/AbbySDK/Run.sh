8d093ffb64b1ab0c23dae3c669b8f4cf2b194c35
akshatgoyalalgo <akshat.goyal@lagonox.com>
2020-02-25 16:41:13 +0530

    abby sdk test

--- template_detection/BL/AbbySDK/Run.sh
+++ template_detection/BL/AbbySDK/Run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/opt/ABBYY/FREngine12/Bin/
-java -classpath ".:bin/.:lib/abbyy.FREngine.jar:lib/mysql-connector-java-8.0.17.jar" com.algonox.abbyy.OCRExtraction $1
+java -classpath ".:bin/.:lib/abbyy.FREngine.jar:lib/mysql-connector-java-8.0.17.jar:lib/json-simple-1.1.jar" com.algonox.abbyy.OCRExtraction $1
